8835e83fac3b27e6636966b268f98ea85edc3724
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 06:59:26 +0300

    Updates

---
+++ precache-manifest.de3774ca2148133cd8ecd99f54b54080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91675e0bcdaa1ecc2154b42cd201108",
+    "url": "/to-do-vanila-React/index.html"
+  },
+  {
+    "revision": "a2bb98051290e0a376c8",
+    "url": "/to-do-vanila-React/static/css/main.5d59cf50.chunk.css"
+  },
+  {
+    "revision": "d4053d3633d76fa971d1",
+    "url": "/to-do-vanila-React/static/js/2.2df73a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-vanila-React/static/js/2.2df73a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bb98051290e0a376c8",
+    "url": "/to-do-vanila-React/static/js/main.e47286b6.chunk.js"
+  },
+  {
+    "revision": "2086f2597421d050ea6d",
+    "url": "/to-do-vanila-React/static/js/runtime-main.0219928b.js"
+  }
+]);
